a00c0e059a5ca200d87dff54e9b990046fc95caf
zhoufangyong <92912138+zhoufangyong@users.noreply.github.com>
2021-11-03 12:53:03 +0800

    Add files via upload

---
+++ TrafficLight.rs
@@ -0,0 +1,22 @@
+﻿enum  TrafficLight{
+    Red,
+    Yellow,
+    Green,
+}
+
+impl TrafficLight{
+    fn time(&self)->i32{
+        match self{
+            TrafficLight::Yellow =>3,
+            _ =>60
+        }
+    }
+}
+
+fn main() {
+  let red_light = TrafficLight::Red;
+  let yellow_light = TrafficLight::Yellow;
+  let geen_light = TrafficLight::Green;
+  println!("Traffic light Seconds:Red:{},Yellow:{},Green:{}",
+    red_light.time(),yellow_light.time(),geen_light.time());
+}
