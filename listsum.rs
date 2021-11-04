472cf26d1d627dda03fc70acd2a07465b1e0b01e
zhoufangyong <92912138+zhoufangyong@users.noreply.github.com>
2021-11-04 10:01:09 +0800

    Add files via upload

--- listsum.rs
+++ listsum.rs
@@ -1,27 +1,31 @@
-﻿fn sum(list: &[u32]) -> Option<u32> {
+﻿fn ListSum(list: &[u32]) -> Option<u32> {
     let mut sum: u32 = 0;
-    for i in list.into_iter() {
-        match sum.checked_add(*i) {
-            Some(data) => sum = data,
-            None => return  None
-        }
+
+    for item in list.iter(){
+        if sum.checked_add(item*1) == None{
+            //println!("overflow");
+            return None;
+        }    
+        sum+=item;
     }
+    //println!("sum:{}",sum);
     Some(sum)
 }
+
 fn main() {
 
     //正常计算，输出和
     let list = vec![1u32,2,3,4,5];
-    println!("计算结果（数字）{}",sum(&list).unwrap());
+    println!("计算结果（数字）{}",ListSum(&list).unwrap());
 
     //正常计算，输出Option
     let list2 = vec![1u32,2,3,4,5];
-    let result2: Option<u32> = sum(&list2);
+    let result2: Option<u32> = ListSum(&list2);
     println!("计算结果（枚举）:{:?}", result2);
     
     //溢出，None
-    let list3 = vec![u32::max_value()-1, 2];
-    let result3: Option<u32> = sum(&list3);
+    let list3 = vec![u32::max_value()-1, 2,3,4,5];
+    let result3: Option<u32> = ListSum(&list3);
     println!("溢出结果（None）:{:?}", result3);
 
 }
